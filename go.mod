32788619e1c6165fbc5770b59f75c24e8eca397a
KhalidShikur <khldshikur@gmail.com>
2025-11-16 01:51:08 +0300

    boilers plate for go http server complete

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/KhalidShikur/My-Go-Server
+
+go 1.25.3
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/cors v1.2.2
---
+++ json.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+func respondWithErr(w http.ResponseWriter, code int, msg string) {
+	type ErrResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJson(w, code, ErrResponse {msg})
+}
+
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed to marshal JSON response: %v", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
